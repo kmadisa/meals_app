42ed552f68a5e3eaedd65cc0278f7f22c2990d6d
Katleho Madisa <katleho.madisa47@gmail.com>
2021-06-26 14:46:09 +0000

    176 Adding a TabBar to the Appbar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,23 +5,18 @@ import 'package:meals_app/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeal'),
+    return GridView(
+      padding: EdgeInsets.all(15),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: EdgeInsets.all(15),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_DATA
-            .map((category) =>
-                CategoryItem(category.id, category.title, category.color))
-            .toList(),
-      ),
+      children: DUMMY_DATA
+          .map((category) =>
+              CategoryItem(category.id, category.title, category.color))
+          .toList(),
     );
   }
 }
